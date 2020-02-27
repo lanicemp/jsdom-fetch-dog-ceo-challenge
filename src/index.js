5b8bc3d563a5ab4f6df14401d327747f7a63c27e
LaNice Powell <lanicemp@aol.com>
2020-02-27 11:49:41 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchImage() {
+     fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderImage(json)); 
+   }
+  
+  function renderImage(json) {
+    const dogContainer = document.querySelector("#dog-image-container")
+    json.message.forEach(image => {
+      const img = document.createElement('img')
+      img.src = image
+      dogContainer.appendChild(img)
+    })
+  }
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+  function fetchDogBreeds() {
+    fetch(breedUrl)
+   .then(resp => resp.json())
+   .then(json => renderBreeds(json)); 
+  }
+
+  function renderBreeds(json) {
+    const dogBreeds = document.querySelector("#dog-breeds")
+    for (const breed in json.message)
+     (`${breed}: ${json.message[breed.name]}`);
+      const li = document.createElement('li')
+      li.innerHTML = dogBreeds
+      dogBreeds.appendChild(li)
+    
+ 
+}
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchImage()
+    fetchDogBreeds()
+  
+    
+  })
+  
