954c1c3f16408692234fb05772298183d3906ed4
LaNice Powell <lanicemp@aol.com>
2020-02-28 12:59:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -8,33 +8,64 @@ function fetchImage() {
     .then(json => renderImage(json)); 
    }
   
-  function renderImage(json) {
-    const dogContainer = document.querySelector("#dog-image-container")
-    json.message.forEach(image => {
-      const img = document.createElement('img')
-      img.src = image
-      dogContainer.appendChild(img)
-    })
-  }
-  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-
-  function fetchDogBreeds() {
-    fetch(breedUrl)
-   .then(resp => resp.json())
-   .then(json => renderBreeds(json)); 
-  }
-
-  function renderBreeds(json) {
-    const dogBreeds = document.querySelector("#dog-breeds")
-    for (const breed in json.message) {
-      const li = document.createElement('li')
-      li.innerHTML = `${breed}`
-      dogBreeds.appendChild(li)
-    }
-}
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    fetchImage()
-    fetchDogBreeds()
+function renderImage(json) {
+  const dogContainer = document.querySelector("#dog-image-container")
+  json.message.forEach(image => {
+    const img = document.createElement('img')
+    img.src = image
+    dogContainer.appendChild(img)
   })
-  
+}
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+function fetchDogBreeds(filter = "") {
+  fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => renderBreeds(json, filter)); 
+}
+
+function renderBreeds(json, filter = "") {
+  const dogBreeds = document.querySelector("#dog-breeds")
+  for (const breed in json.message) {
+    const li = document.createElement('li')
+    li.innerHTML = `${breed}`
+    if (filter== ""){
+      dogBreeds.appendChild(li)  
+    }else {
+      if (breed[0] === filter){
+      dogBreeds.appendChild(li)
+      }
+    }
+  }
+}
+    
+document.addEventListener('DOMContentLoaded', function() {
+  fetchImage()
+  fetchDogBreeds()
+  const ul = document.querySelector("ul")
+  ul.addEventListener("click", function(event){
+    let element = event.target 
+    switchColors(element)
+  } )
+    const select = document.querySelector("#breed-dropdown")
+    select.addEventListener("change", function(){
+      let filter = select.value
+      fetchDogBreeds(filter)
+      //call fetch breed method and pass in value as an argument
+     })
+})
+
+function switchColors(element){  
+  links=document.getElementsByTagName("li") ;  
+  for (var i = 0 ; i < links.length ; i ++){
+    links.item(i).style.color = 'black' ; 
+   }
+  element.style.color='orange' ;  
+}  
+
+//dogbreeds.innertext 
+// returns the values of all the dogs. 
+// const dogBreednames = dogBreeds.innerText
+  //user selects the letter we ge the value and then fetch the breeds render the
+  // breeds on the page 
+  //add code to the render breed function that only return the letter o
